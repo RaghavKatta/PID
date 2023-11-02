098892a143e9fa84f9279a8839b563f04064fc6d
RaghavKatta <145485603+RaghavKatta@users.noreply.github.com>
2023-11-01 23:01:15 -0500

    Update rotational_tuning.py

--- rotational_tuning.py
+++ rotational_tuning.py
@@ -49,7 +49,7 @@ with col1:
 with col2:
     Ki = st.number_input('Ki', min_value=0.0, max_value=10000000.0, value=0.0, step=0.01)
     #CODE WILL STILL USE VARIABLE MASS SO i DON'T NEED TO GO BACK AND CHANGE IT FROM THE LINEAR SET UP
-    Mass = st.number_input("Inertia(kg*m^2)", min_value = 1.0, max_value = 100.00, step = .01)
+    Mass = st.number_input("Inertia(kg*m^2)", min_value = 1.0, max_value = 500.00, step = .01)
 
 # Input fields in the third column
 with col3:
@@ -113,9 +113,7 @@ y_values2 = equation2(x_range, Kp, Ki, Kd, seconds_delay, Disturbance, Mass, Max
 fig, ax = plt.subplots()
 def setup(fig,ax):
     ax.plot(x_range, y_values, label = 'signal', color = 'blue')
-    ax.plot(x_range, y_values2, label='Line 2', color='red')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
+    ax.plot(x_range, y_values2, label='PID Results', color='red')
     ax.set_title('PID tuning stuff')
     fig.set_facecolor('black')
     ax.set_facecolor('black')
@@ -128,6 +126,7 @@ def setup(fig,ax):
     ax.yaxis.set_tick_params(color='white', labelcolor='white')
     ax.set_ylim(0)
     ax.set_xlim(0)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.1, -0.07), frameon=False, labelcolor = "white")
 
 # Display the Matplotlib figure using Streamlit
 setup(fig,ax)
@@ -339,3 +338,197 @@ if st.button("Generate BETTER PID (Precise!!)(4-5)"):
     end_time = time.time()
     st.text("extra precise estimate: " + str(extra_precise_estimate[0]) + " Mass adjusted estimate: " + str(extra_precise_estimate[1]) + " Score: " + str(extra_precise_estimate[2]))
     st.text("time: " + str(end_time-start_time))
+
+##NOW WE ARE DISPLAYING THE BEST PID VALUES AS THE INERTIA AND THRUST CHANGE
+#we can create an insert for starting and ending inertia 
+inertia_start = st.number_input('inertia_start', min_value=0.0, max_value=1000.0, value=1.0, step=.0001, format="%.4f")
+inertia_end = st.number_input('inertia_end', min_value=0.0, max_value=1000.0, value=1.0, step=.0001, format="%.4f")
+inertia_difference = inertia_end - inertia_start
+#initalize the points for this rn
+seconds_delay_plot = st.number_input('Delay Plot', min_value=0, max_value=1000, value=10)
+Disturbance_plot = st.number_input("Disturbance Plot", min_value= -1000.00, max_value = 1000.0, value = 0.0, step = .0001, format="%.4f")
+inertia_range = np.linspace(0, 7, 28)
+
+#here we have to use the code for inertia and thrust though(no fun inserts)
+def Inertia(time):
+    # time in ms
+    # check my math on this, I put the base equations in the notes
+    # Inertia_difference is the difference between initial and final inertia
+    # fuel fraction is the integral of the fuel used up, but it's not divided yet
+    # we have to do that at the end, which is why it is divided by 961.8 at the end
+    t = time
+    fuel_fraction = 0
+    if t < 0.125:
+        fuel_fraction = (-1300/3) * (t)**3 + 350 * t + t**2 
+    elif t < 2.125:
+        fuel_fraction = 250 *(t-.125) - 15 * (t-.125)**2 + 35.341145+2.5
+    elif t < 3.5:
+        fuel_fraction = 190 * (t-2.125) - (9/2) * (t - 2.125)**2 + 475.3411 + 2.5
+    elif t < 5.4:
+        fuel_fraction = 177.6 * (t-3.5) - 35 * (t - 3.5)**2 + 728.08333 + 2.5
+    elif t < 6.4:
+        fuel_fraction = 44.6 * (t-5.4) - 22 * (t - 5.4)**2 + 939.1733333 + 2.5
+    else:
+        fuel_fraction = 961.8 + 2.5
+
+    return inertia_difference * fuel_fraction / (961.8 + 2.5) + inertia_start
+
+
+def thrust(time):
+    # time in ms
+    # check my math on this, I put the base equations in the notes
+    # Inertia_difference is the difference between initial and final inertia
+    # fuel fraction is the integral of the fuel used up, but it's not divided yet
+    # we have to do that at the end, which is why it is divided by 961.8 at the end
+    t = time
+    thrust = 0
+    if t < 0.125:
+        thrust = -1300 * (t + 0.125)**2 + 350 + 2 * t
+    elif t < 2.125:
+        thrust = 250 - 30 * (t - 0.125)
+    elif t < 3.5:
+        thrust = 190 - 9 * (t - 2.125)
+    elif t < 5.4:
+        thrust = 177.6 - 70 * (t - 3.5)
+    elif t < 6.4:
+        thrust = 44.6 - 44 * (t - 5.4)
+    else:
+        thrust = 0
+
+    return thrust
+
+
+inertia_vals = []
+for x in inertia_range:
+    inertia_vals.append(Inertia(x))
+
+thrust_vals = []
+for x in inertia_range:
+    thrust_vals.append(thrust(x))
+
+
+st.title("")
+# Create a layout with four columns
+col1, col2 = st.columns(2)
+with col1:
+    st.title("Thrust Over Time")
+    display, axis = plt.subplots()
+    def setupPIDdisplay(fig,ax):
+        fig.set_facecolor('black')
+        ax.set_facecolor('black')
+        ax.set_xlabel('time(s)', color='white')
+        ax.set_ylabel('PID values', color='white')  
+        ax.title.set_color('white') 
+        ax.spines['bottom'].set_color('white')  # Change the color of the x-axis line to white
+        ax.spines['left'].set_color('white')    # Change the color of the y-axis line to white
+        ax.xaxis.set_tick_params(color='white', labelcolor='white')
+        ax.yaxis.set_tick_params(color='white', labelcolor='white')
+
+    # Display the Matplotlib figure using Streamlit
+    setupPIDdisplay(display,axis)
+    ax.set_title("Thrust")
+    axis.plot(inertia_range, thrust_vals, label = 'Thrust', color = 'RED')
+    axis.set_ylim(0,350)
+    axis.set_xlim(0,7)
+    st.pyplot(display)
+
+    
+# Input fields in the second column
+with col2:
+    st.title("Inertia Over Time")
+
+    display, axis = plt.subplots()
+    # Display the Matplotlib figure using Streamlit
+    setupPIDdisplay(display,axis)
+    axis.plot(inertia_range, inertia_vals, label = 'Inertia', color = 'blue')
+    axis.set_xlim(0,7)
+    st.pyplot(display)
+
+@st.cache_data
+def generate_best_plot(Kpi, Kpf, Kpg, Kii, Kif, Kig, Kdi, kdf, Kdg, mass, max_force):
+    #we divide by 10 for Kp and Ki so that when we do the precise one we can use decimals
+    min_score = 1000
+    best_vals = []
+    for p in range(Kpi, Kpf, Kpg):
+        Kp_tune = p/100
+        for i in range(Kii, Kif, Kig):
+            Ki_tune = i/100
+            for d in range(Kdi, kdf, Kdg):
+                Kd_tune = d/100
+                tuning_calcs = make_calculations(inertia_range, Kp_tune, Ki_tune, Kd_tune, seconds_delay_plot, Disturbance_plot/mass, x_points, 1, max_force/mass)
+                score = get_score(tuning_calcs[0]-1, tuning_calcs[1]/(x_end-x_start), tuning_calcs[2]-1, tuning_calcs[3]/x_points * (x_end-x_start))
+                if score < min_score:
+                    min_score = score
+                    best_vals = [Kp_tune, Ki_tune, Kd_tune]
+    raw_vals = best_vals.copy()
+    for i in range(len(best_vals)):
+        best_vals[i] = best_vals[i] * mass
+    return raw_vals, best_vals, min_score
+
+@st.cache_data
+def generate_best_pid(inertia_vals, thrust_vals):
+    P_values_plot = []
+    I_values_plot = []
+    D_values_plot = []
+    min_scores_plot = []
+    for x in range(28):
+        inertia_plot = inertia_vals[x]
+        thrust_plot = thrust_vals[x]
+        rough_estimate = generate_best_plot(0, 10000, 1000, 0, 10000, 1000, 0, 100000, 1000, inertia_plot, thrust_plot)
+        P_values_plot.append(rough_estimate[1][0])
+        I_values_plot.append(rough_estimate[1][1])
+        D_values_plot.append(rough_estimate[1][2])
+        min_scores_plot.append(rough_estimate[2])
+
+    return P_values_plot, I_values_plot, D_values_plot, min_scores_plot
+
+
+st.title("PID overtime")
+start_time = time.time()
+PID_values_plot = generate_best_pid(inertia_vals, thrust_vals)
+pid_display, pid_axis = plt.subplots()
+setupPIDdisplay(pid_display, pid_axis)
+pid_axis.set_title("P Values")
+P_values_plot = []
+end_time = time.time()
+st.text(PID_values_plot[0])
+P_values_plot = PID_values_plot[0]
+st.text(end_time-start_time)
+#rough_estimate = generate_best_plot(0, 10000, 1000, 0, 10000, 1000, 0, 100000, 1000, 10, 1000)
+pid_axis.plot(inertia_range, P_values_plot, label = 'P', color = 'blue')
+pid_axis.set_xlim(0,7)
+st.pyplot(pid_display)
+
+
+
+pid_display, pid_axis = plt.subplots()
+setupPIDdisplay(pid_display, pid_axis)
+pid_axis.set_title("I Values")
+st.text(PID_values_plot[0])
+I_values_plot = PID_values_plot[1]
+#rough_estimate = generate_best_plot(0, 10000, 1000, 0, 10000, 1000, 0, 100000, 1000, 10, 1000)
+pid_axis.plot(inertia_range, I_values_plot, label = 'I', color = 'red')
+pid_axis.set_xlim(0,7)
+st.pyplot(pid_display)
+
+pid_display, pid_axis = plt.subplots()
+setupPIDdisplay(pid_display, pid_axis)
+pid_axis.set_title("D Values")
+st.text(PID_values_plot[0])
+D_values_plot = PID_values_plot[2]
+#rough_estimate = generate_best_plot(0, 10000, 1000, 0, 10000, 1000, 0, 100000, 1000, 10, 1000)
+pid_axis.plot(inertia_range, D_values_plot, label = 'D', color = 'white')
+pid_axis.set_xlim(0,7)
+st.pyplot(pid_display)
+
+pid_display, pid_axis = plt.subplots()
+setupPIDdisplay(pid_display, pid_axis)
+pid_axis.set_title("Score Values")
+st.text(PID_values_plot[0])
+score_values_plot = PID_values_plot[3]
+st.text(end_time-start_time)
+#rough_estimate = generate_best_plot(0, 10000, 1000, 0, 10000, 1000, 0, 100000, 1000, 10, 1000)
+pid_axis.plot(inertia_range, score_values_plot, label = 'Score', color = 'white')
+pid_axis.set_xlim(0,7)
+st.pyplot(pid_display)
+
